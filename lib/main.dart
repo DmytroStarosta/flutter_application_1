8459931f46631c784cfcd04d5e79c4d8379771fd
Dmytro Starosta <lviv062006@gmail.com>
2026-03-17 01:32:38 +0200

    Fix main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,30 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:flutter_application_1/pages/registration_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SmartMeteoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SmartMeteoApp extends StatelessWidget {
+  const SmartMeteoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Smart Meteo Station',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({required this.title, super.key});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController controller = TextEditingController();
-  String text = 'Тут може бути ваша реклама';
-  String inputText = '';
-  Color currentColor = Colors.deepPurple;
-
-  void _changeText() {
-    setState(() {
-      final String trimmedInput = inputText.trim();
-      if (trimmedInput.isNotEmpty) {
-        if (trimmedInput.toLowerCase() == 'reset') {
-          text = 'Тут може бути ваша реклама';
-          currentColor = Colors.deepPurple;
-        } else {
-          text = inputText;
-          currentColor = Color(
-            (Random().nextDouble() * 0xFFFFFF).toInt(),
-          ).withValues(alpha: 1);
-        }
-        controller.clear();
-        inputText = '';
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: currentColor,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Banner:'),
-            Text(
-              text,
-              style: TextStyle(
-                color: currentColor,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: 400,
-              height: 70,
-              child: TextField(
-                controller: controller,
-                onChanged: (value) {
-                  setState(() {
-                    inputText = value;
-                  });
-                },
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Введіть вашу рекламу',
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: _changeText,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: currentColor,
-              ),
-              child: const Icon(
-                Icons.done,
-                color: Colors.white,
-              ),
-            )
-          ],
-        ),
-      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(), 
+        '/home': (context) => const LoginScreen(),
+        '/profile': (context) => const LoginScreen(),
+      },
     );
   }
 }
